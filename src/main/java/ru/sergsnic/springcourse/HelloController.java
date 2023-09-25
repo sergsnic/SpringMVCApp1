fb4ab03814e4567a1b2b9899356f24a72ca31e1a
Sergey <sergsnic@mail.ru>
2023-09-25 14:42:02 +0300

    add Project SpringMVCApp1 Task 15 - 16.

---
+++ src/main/java/ru/sergsnic/springcourse/HelloController.java
@@ -0,0 +1,15 @@
+package ru.sergsnic.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Neil Alishev
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
