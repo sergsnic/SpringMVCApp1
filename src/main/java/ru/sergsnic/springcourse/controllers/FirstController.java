a6c6de376198a4f2792c07fa5ffdfa746d95330d
Sergey <sergsnic@mail.ru>
2023-09-28 14:16:22 +0300

    add Task20 Model

--- src/main/java/ru/sergsnic/springcourse/controllers/FirstController.java
+++ src/main/java/ru/sergsnic/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.sergsnic.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/first")
 public class FirstController {
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "numberA", required = false) Integer numberA,
+                                 @RequestParam(value = "numberB", required = false) Integer numberB,
+                                 @RequestParam("arithmeticOperation") String arithmeticOperation, Model model){
+
+        // System.out.println("What are you doing? " + arithmeticOperation);
+        try {
+            switch (arithmeticOperation) {
+                case "multiplication":
+                    model.addAttribute("message",
+                            numberA + " * " + numberB + " = " + (numberA * numberB));
+                    break;
+                case "addition":
+                    model.addAttribute("message",
+                            numberA + " + " + numberB + " = " + (numberA + numberB));
+                    break;
+                case "substraction":
+                    model.addAttribute("message",
+                            numberA + " - " + numberB + " = " + (numberA - numberB));
+                    break;
+                case "division":
+                    model.addAttribute("message",
+                            numberA + " / " + numberB + " = " + (numberA / (double)numberB));
+                    break;
+                default:
+                    break;
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return "first/calculator";
+    }
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
--- src/main/java/ru/sergsnic/springcourse/controllers/SecondController.java
+++ src/main/java/ru/sergsnic/springcourse/controllers/SecondController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/second")
+//@Controller
+//@RequestMapping("/second")
 public class SecondController {
-
-    @GetMapping("/exit")
-    public String exit() {
-        return "second/exit";
-    }
+//
+//    @GetMapping("/exit")
+//    public String exit() {
+//        return "second/exit";
+//    }
 }
